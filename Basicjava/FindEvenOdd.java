1c9fd5bfbe05d7a2f70530a5c91715d8b9851b77
Rakesh829473 <rk5271628@gmail.com>
2026-04-22 12:02:12 +0530

    findevenOdd

---
+++ Basicjava/FindEvenOdd.java
@@ -0,0 +1,18 @@
+
+public class FindEvenOdd
+ {
+
+    public static void main (String[] args)
+    {
+        int a=-10;
+        if(a%2==0)
+        {
+                 System.out.println("even number");
+        }
+        else
+        {
+             System.out.println("oddnumber");
+        }
+    }
+    
+}
