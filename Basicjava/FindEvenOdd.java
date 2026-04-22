d89cc9e482e33a1fb5b37597f33b693af06ecb4a
Rakesh829473 <rk5271628@gmail.com>
2026-04-22 12:05:00 +0530

    fristcode

--- Basicjava/FindEvenOdd.java
+++ Basicjava/FindEvenOdd.java
@@ -4,7 +4,7 @@ public class FindEvenOdd
 
     public static void main (String[] args)
     {
-        int a=-10;
+        int a=-12;
         if(a%2==0)
         {
                  System.out.println("even number");
