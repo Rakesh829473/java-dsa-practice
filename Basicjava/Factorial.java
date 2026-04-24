2b6e7c837bf6ad1c1bc3959f335bdc6be5d147aa
Rakesh829473 <rk5271628@gmail.com>
2026-04-25 01:24:53 +0530

    factorial

---
+++ Basicjava/Factorial.java
@@ -0,0 +1,20 @@
+public class Factorial 
+{   
+    public static int factorial(int n)
+    {
+        if(n==0)
+        {
+            return 1;
+        }
+         return  factorial(n-1)*n;
+
+    }
+    public static void main(String[] args)
+    {
+        int n=5;
+        factorial(n);
+        System.out.println(factorial(n));
+
+    }
+    
+}
