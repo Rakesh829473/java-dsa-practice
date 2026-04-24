c772cebd6c3475a53fb42983138cd2c62c589d09
Rakesh829473 <rk5271628@gmail.com>
2026-04-25 00:07:28 +0530

    largest number

---
+++ Basicjava/loftn.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class loftn
+ {
+
+    public static void main (String[] args)
+    {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter  1st number");
+           int a = sc.nextInt();
+        System.out.println("Enter 2nd number");
+        int b=sc.nextInt();
+        System.out.println("Enter 3rd number");
+        int c=sc.nextInt(); 
+
+        if(a>=b && a>=c)
+        {
+            System.out.println("Largest = " + a);
+        }
+
+        else if (b>=a && b>=c)
+         {
+            System.out.println("Largest = " + b);
+            
+        } else {
+            System.out.println("Largest = " + c);
+        }
+
+
+       sc.close();
+
+      
+    }
+}
